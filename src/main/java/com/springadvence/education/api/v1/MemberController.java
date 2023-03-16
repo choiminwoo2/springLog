d4c2a86123abc7c4e6a30c712aa1fea4432f0475
wjrdjs005 <wjrdjs005@naver.com>
2023-03-16 23:20:14 +0900

    '로컬쓰레드를사용한로그처리'

--- src/main/java/com/springadvence/education/api/v1/MemberController.java
+++ src/main/java/com/springadvence/education/api/v1/MemberController.java
@@ -1,5 +1,7 @@
 package com.springadvence.education.api.v1;
 
+import com.springadvence.education.config.log.ThreadLocalLogProvider;
+import com.springadvence.education.config.log.TraceStatus;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -12,11 +14,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 
     private final MemberService memberService;
-
+    private final ThreadLocalLogProvider trace;
     @GetMapping("v1/member")
-    public String insertMember(){
-        log.info("MemberController.insertMember()");
-        memberService.save();
+    public String insertMember(String userId){
+        TraceStatus status = null;
+        try{
+            status = trace.begin("MemberController.insertMember()");
+            memberService.save(userId);
+            trace.end(status);
+        }catch (Exception e){
+            trace.exception(status, e);
+            throw e;
+        }
 
         return "ok";
     }
--- src/main/java/com/springadvence/education/api/v1/MemberRepository.java
+++ src/main/java/com/springadvence/education/api/v1/MemberRepository.java
@@ -1,5 +1,7 @@
 package com.springadvence.education.api.v1;
 
+import com.springadvence.education.config.log.ThreadLocalLogProvider;
+import com.springadvence.education.config.log.TraceStatus;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
@@ -9,7 +11,29 @@ import org.springframework.stereotype.Repository;
 @Slf4j
 public class MemberRepository {
 
-    public void save() {
-        log.info("MemberRepository.save()");
+    private final ThreadLocalLogProvider trace;
+
+    public void save(String userId) {
+
+        TraceStatus status = null;
+        try {
+            status = trace.begin("MemberRepository.save()");
+            if (userId.equals("ex")) {
+                throw new IllegalStateException("예외 발생!");
+            }
+            sleep(1000);
+            trace.end(status);
+        }catch (Exception e){
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+
+    private void sleep(int mils){
+        try {
+            Thread.sleep(mils);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
